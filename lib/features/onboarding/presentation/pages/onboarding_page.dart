99e435fbfa03e8088e7ff7496dca3df92b80bfcd
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-09 17:56:08 +0100

    timer to onboarding

--- lib/features/onboarding/presentation/pages/onboarding_page.dart
+++ lib/features/onboarding/presentation/pages/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm/core/presentation/resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -10,6 +11,9 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: ColorManager.white,
+      body: Center(child: Text("Welcome to on boarding")),
+    );
   }
 }
--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mvvm/core/presentation/resources/aseets_manager.dart';
 import 'package:mvvm/core/presentation/resources/color_manager.dart';
+import 'package:mvvm/core/routes/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,6 +13,28 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  void _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
