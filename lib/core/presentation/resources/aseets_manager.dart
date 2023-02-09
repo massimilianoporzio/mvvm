e0e758bf11b1194d45ece90cadf2b73e81eba806
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-02-09 21:57:28 +0100

    assets for onboarding

--- lib/core/presentation/resources/aseets_manager.dart
+++ lib/core/presentation/resources/aseets_manager.dart
@@ -2,4 +2,8 @@ const String IMAGE_PATH = 'assets/images';
 
 class ImageAssets {
   static const String splashLogo = '$IMAGE_PATH/splash_logo.png';
+  static const String onboarding1 = '$IMAGE_PATH/onboarding_logo1.svg';
+  static const String onboarding2 = '$IMAGE_PATH/onboarding_logo2.svg';
+  static const String onboarding3 = '$IMAGE_PATH/onboarding_logo3.svg';
+  static const String onboarding4 = '$IMAGE_PATH/onboarding_logo4.svg';
 }
