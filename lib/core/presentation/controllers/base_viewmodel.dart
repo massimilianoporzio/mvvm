5b9241c8d11d43e3e11f0e53a36980bb489e47d6
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-10 09:38:48 +0100

    base viewmodel

--- lib/core/presentation/controllers/base_viewmodel.dart
+++ lib/core/presentation/controllers/base_viewmodel.dart
@@ -1,1 +1,14 @@
-abstract class BaseViewModel {}
+abstract class BaseViewModel {
+  //*shared variables and methods used by any view model
+  //HA UN INPUT E UN OUPUT
+}
+
+abstract class BaseViewModelInput {
+  //*input from the view
+  void start(); //*init of the viewmodel
+  void dispose(); //*when viewmodel dies (es: navigo su altra view)
+}
+
+abstract class BaseViewModelOutput {
+  //*output FOR the view
+}
